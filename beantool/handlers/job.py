9356a4514ae895caa89ba9f15b6f42eb49b3ac03
Dustin Oprea <myselfasunder@gmail.com>
2014-01-29 19:31:57 -0500

    Implemented job-reserve.
    Refactored a little. Added to-do's.

--- beantool/handlers/job.py
+++ beantool/handlers/job.py
@@ -13,7 +13,8 @@ def register_commands(subparsers):
     parser_jobput.add_argument('data', help='Job data')
 
     parser_jobreserve = subparsers.add_parser('job_reserve', help='Reserve a job')
-    parser_jobreserve.add_argument('-w', '--watch', action='append', help='Tube(s) to take from')
+    parser_jobreserve.add_argument('-t', '--tube', action='append', help='Tube(s)')
+    parser_jobreserve.add_argument('-T', '--timeout', type=int, help='Timeout')
 
     parser_jobpeek = subparsers.add_parser('job_peek', help='Peek on a job')
     parser_jobpeek.add_argument('job_id', type=int, help='ID of specific job')
@@ -26,6 +27,8 @@ def register_commands(subparsers):
 
     parser_jobpeekburied = subparsers.add_parser('job_peek_buried', help="Peek for 'buried' jobs")
     parser_jobpeekburied.add_argument('-t', '--tube', help='Tube')
+
+# TODO(dustin): All informative output should go to STDERR. All data should go to STDOUT.
 
 
 class JobHandler(HandlerBase):
@@ -40,16 +43,27 @@ class JobHandler(HandlerBase):
         j = self.get_job_by_id(job_id)
 
         j = WrappedJob(j)
-        self.__dump_peek_result(j)
+        self.__dump_job(j)
 
-    def __dump_peek_result(self, j):
+    def __dump_job(self, j):
         print(j)
         print('')
         print(j.body)
 
-    def peek_ready(self, tube=None):
+    def __use(self, tube):
         if tube is not None:
             self.beanstalk.use(tube)
+
+    def __watch(self, tubes):
+        if tubes is not None:
+            if issubclass(tubes.__class__, (list, tuple)) is False:
+                tubes = [tubes]
+
+                for tube in tubes:
+                    self.beanstalk.watch(tube)
+
+    def peek_ready(self, tube=None):
+        self.__use(tube)
 
         j = self.beanstalk.peek_ready()
         if j is None:
@@ -57,11 +71,10 @@ class JobHandler(HandlerBase):
             return
 
         j = WrappedJob(j)
-        self.__dump_peek_result(j)
+        self.__dump_job(j)
 
     def peek_delayed(self, tube=None):
-        if tube is not None:
-            self.beanstalk.use(tube)
+        self.__use(tube)
 
         j = self.beanstalk.peek_delayed()
         if j is None:
@@ -69,11 +82,10 @@ class JobHandler(HandlerBase):
             return
 
         j = WrappedJob(j)
-        self.__dump_peek_result(j)
+        self.__dump_job(j)
 
     def peek_buried(self, tube=None):
-        if tube is not None:
-            self.beanstalk.use(tube)
+        self.__use(tube)
 
         j = self.beanstalk.peek_buried()
         if j is None:
@@ -81,4 +93,24 @@ class JobHandler(HandlerBase):
             return
 
         j = WrappedJob(j)
-        self.__dump_peek_result(j)
+        self.__dump_job(j)
+
+    def reserve(self, tube=None, timeout=None):
+        """Allocate a job to work on. A timeout of None means that the call 
+        will block.
+        """
+
+        self.__watch(tube)
+# TODO(dustin): It seems like we can call this many times, rapidly, and get the same job back. Why?
+        j = self.beanstalk.reserve(timeout)
+        if j is None:
+            print("No jobs to reserve.")
+            return
+
+        j = WrappedJob(j)
+        self.__dump_job(j)
+
+# TODO(dustin): Add a "delete" call.
+# TODO(dustin): Add a "stats" call.
+# TODO(dustin): Add a "release" call.
+# TODO(dustin): Add a "bury" call.
