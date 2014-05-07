556ffa3a95efdcc1c7ea962e0ee9ddea7d82d127
Dustin Oprea <myselfasunder@gmail.com>
2014-05-07 01:14:14 -0400

    Added license stubs.

--- beantool/job_terminal.py
+++ beantool/job_terminal.py
@@ -1,3 +1,11 @@
+# beantool: Beanstalk console client.
+# Copyright (C) 2014  Dustin Oprea
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
 import re
 import json
 import phpserialize
